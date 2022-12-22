01aa9ad849f8a9a2c77e9e629be955029c1b2c4b
chinonso ebere <chinonso.ebere2017@gmail.com>
2022-12-22 15:06:53 -0500

    started a server on port 3000

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+'use strict';
 /**
+ * Primary file for API
  *
  */
+
+// Dependency
+const http = require('http');
+
+//The server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+  res.end('Hello World!!!');
+});
+
+//Start the server and listen on port 3000
+server.listen(3000, () => {
+  console.log('server is listening on port 3000');
+});
