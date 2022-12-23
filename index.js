308f128a970f9a44e527dfab50b93ebfa620ba7a
chinonso ebere <chinonso.ebere2017@gmail.com>
2022-12-23 11:44:12 -0500

    Obtaining the Payload

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
 
 // Dependency
 const http = require('http');
+const { StringDecoder } = require('string_decoder');
 const url = require('url');
+const stringDecoder = require('string_decoder').StringDecoder;
 
 //The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
@@ -25,12 +27,22 @@ const server = http.createServer((req, res) => {
   //Get the HTTP Methods
   const method = req.method.toLowerCase();
 
-  //Send Response
-  res.end('Hello World!!!\n');
+  //Get the payload
+  const decoder = new StringDecoder('utf-8');
+  let payload = '';
+  req.on('data', (data) => {
+    payload += decoder.write(data);
+  });
+  req.on('end', () => {
+    payload += decoder.end();
 
-  //Log the URL Path
-  //   console.log(`${queryStringObject} ${path} ${method}`);
-  console.log(headers);
+    //Send Response
+    res.end('Hello World!!!\n');
+
+    //Log the URL Path
+    // console.log(`${path} ${method}`);
+    console.log(payload);
+  });
 });
 
 //Start the server and listen on port 3000
