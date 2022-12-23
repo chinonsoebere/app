bc188c7c7cfbaf08724badecaf5b0930e24177d3
chinonso ebere <chinonso.ebere2017@gmail.com>
2022-12-23 10:52:09 -0500

    Parsed HTTP Methods

--- index.js
+++ index.js
@@ -6,10 +6,24 @@
 
 // Dependency
 const http = require('http');
+const url = require('url');
 
 //The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-  res.end('Hello World!!!');
+  //Get the URL and Parse it
+  const parsedURL = url.parse(req.url, true);
+
+  //Get the URL Path
+  const path = parsedURL.pathname.replace(/^\/+|\/+$/g, ''); //trimmed
+
+  //Get the HTTP Methods
+  const method = req.method.toLowerCase();
+
+  //Send Response
+  res.end('Hello World!!!\n');
+
+  //Log the URL Path
+  console.log(path, method);
 });
 
 //Start the server and listen on port 3000
